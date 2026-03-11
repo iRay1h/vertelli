067cc9c4b42214f290090428fb358273c82c8936
Jhoan2008 <rodriguez.smith.2008@gmail.com>
2026-03-11 01:04:36 +0000

    Feature:Creación de DTO y mejora de repository

--- src/main/java/com/vertelli/inventory/repository/MotorbikeRepository.java
+++ src/main/java/com/vertelli/inventory/repository/MotorbikeRepository.java
@@ -1,5 +1,8 @@
 package com.vertelli.inventory.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class MotorbikeRepository {
+import com.vertelli.inventory.entity.MotorbikeEntity;
+
+public interface MotorbikeRepository extends JpaRepository<MotorbikeEntity, Long>{
     
 }
